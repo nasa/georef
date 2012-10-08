38079c721224276a536f490aa237a026d2241db3
Ted Scharff <e.b.scharff@nasa.gov>
2012-10-08 15:34:15 -0700

    Root URL routes to backbone.

--- apps/mapFastenApp/urls.py
+++ apps/mapFastenApp/urls.py
@@ -9,6 +9,6 @@ from django.conf.urls.defaults import url, patterns
 urlpatterns = patterns(
     'mapFastenApp.views',
 
-    url(r'^$', 'home',
+    url(r'^$', lambda request: redirect(reverse('geocamTiePoint_backbone')),
         {}, 'mapFasten_home'),
 )
