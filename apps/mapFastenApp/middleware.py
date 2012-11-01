d521b9f9f53b99ad1191d7b5bf6d848c0332b0cc
Trey Smith <trey.smith@nasa.gov>
2012-10-31 19:15:54 -0700

    actually moved home page outside security wall

--- apps/mapFastenApp/middleware.py
+++ apps/mapFastenApp/middleware.py
@@ -9,7 +9,7 @@ from django.http import HttpResponseForbidden
 
 class ClosedBetaMiddleware(object):
     def process_request(self, request):
-        if request.path.startswith('/public'):
+        if request.path.startswith('/public') or request.path == '/':
             return None
         if getattr(request, 'user', None) is None or request.user.is_anonymous():
             return HttpResponseForbidden('401 Sorry, MapFasten is in closed beta and you do not appear to be on the list of authorized testers.')
