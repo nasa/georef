9736ba4fd3d02955182dfe757ca74b1e22a3c465
Grace Lee <yeongrace.lee@gmail.com>
2015-11-17 17:03:17 -0800

    setup.py changed geoRef to georef

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read_file(filename):
         return ''
 
 # Use the docstring of the __init__ file to be the description
-#DESC = " ".join(__import__('geoRef').__doc__.splitlines()).strip()
+#DESC = " ".join(__import__('georef').__doc__.splitlines()).strip()
 DESC = ""
 
 PROJ_ROOT = op.abspath(op.dirname(__file__))
@@ -173,8 +173,8 @@ class MediaCommand(Command):
 
 
 setup(
-    name="geoRef",
-    version='1.0',  # __import__('geoRef').get_version().replace(' ', '-'),
+    name="georef",
+    version='1.0',  # __import__('georef').get_version().replace(' ', '-'),
     url='',
     author='mfsmith3, ylee8',
     author_email='',
