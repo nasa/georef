0fa92acda615e835b3695c608e50bd1b2224b466
Grace Lee <grace.lee@nasa.gov>
2016-04-25 22:20:53 +0000

    moved database script to scripts dir

---
+++ scripts/updateOverlayExtras.py
@@ -0,0 +1,41 @@
+import django
+from django.conf import settings
+django.setup()
+
+from geocamTiePoint.models import Overlay, ISSimage
+from geocamUtil import registration as register
+
+def updateOverlayExtras():
+    """
+    This is a standalone method used to update the contents of overlay extras, 
+    which has changed to reflect the new UI for list overlays page.
+    """
+    overlays = Overlay.objects.all()
+    for overlay in overlays:
+        if overlay.extras.imageSize[0] > 1000:
+            imageSize = 'large'
+        else: 
+            imageSize = 'small'
+        issMRF = overlay.imageData.issMRF
+        if issMRF: 
+            issMRF = issMRF.split('-')
+            mission = issMRF[0]
+            roll = issMRF[1]
+            frame = issMRF[2]   
+            issImage = ISSimage(mission, roll, frame, imageSize)
+            centerPtDict = register.getCenterPoint(issImage)
+            try:
+                overlay.extras.centerLat = round(centerPtDict["lat"],2)
+                overlay.extras.centerLon = round(centerPtDict["lon"],2)
+            except:
+                overlay.extras.centerLat = None
+                overlay.extras.centerLon = None
+            overlay.extras.nadirLat = issImage.extras.nadirLat
+            overlay.extras.nadirLon = issImage.extras.nadirLon
+            ad = issImage.extras.acquisitionDate
+            overlay.extras.acquisitionDate = ad[:4] + ':' + ad[4:6] + ':' + ad[6:] # convert YYYYMMDD to YYYY:MM:DD 
+            at = issImage.extras.acquisitionTime
+            overlay.extras.acquisitionTime = at[:2] + ':' + ad[2:4] + ':' + ad[4:6] # convert HHMMSS to HH:MM:SS
+            overlay.extras.focalLength_unitless = issImage.extras.focalLength_unitless
+            overlay.save()
+updateOverlayExtras()
