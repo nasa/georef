0c38991fb9e4053e842459c16c06c097ab4a3e38
Grace Lee <grace.lee@nasa.gov>
2016-07-14 21:17:10 +0000

    new script added to generate Aligned Quad Tree

---
+++ scripts/generateAlignedQuadTree.py
@@ -0,0 +1,18 @@
+import django
+from django.conf import settings
+django.setup()
+
+from geocamTiePoint.models import *
+
+
+"""
+Adds image width, height, sizeType fields to Image Data (previously was in overlay.extras)
+"""
+def generateAlignedQuadTree():
+    overlays = Overlay.objects.all()
+    for overlay in overlays:
+        if overlay.alignedQuadTree is None:
+            overlay.generateAlignedQuadTree()
+            overlay.save()
+            
+generateAlignedQuadTree()
