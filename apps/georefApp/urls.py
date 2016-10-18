124cd12e02a03709b424f6741f94b29220e12e26
Tamar Cohen <tamar.cohen@nasa.gov>
2016-10-18 09:51:17 -0700

    added error templates and added overview link to header

--- apps/georefApp/urls.py
+++ apps/georefApp/urls.py
@@ -5,7 +5,16 @@
 # __END_LICENSE__
 
 from django.conf.urls import url, include
+from django.conf.urls import handler400, handler403, handler404, handler500
+from django.views.generic.base import TemplateView
 from georefApp import views
 
+# handler400 = 'my_app.views.bad_request'
+# handler403 = 'my_app.views.permission_denied'
+# handler404 = 'my_app.views.page_not_found'
+# handler500 = 'my_app.views.server_error'
+
 urlpatterns = [url(r'^$', views.home, {}, 'georef_home'), 
-               url(r'^georefV2demo/$', views.osdDemoPage, {}, 'georef_osd_demo')] 
+               url(r'^georefV2demo/$', views.osdDemoPage, {}, 'georef_osd_demo'),
+               url(r'^error', TemplateView.as_view(template_name='error.html'), {}, 'error'),
+               ] 
